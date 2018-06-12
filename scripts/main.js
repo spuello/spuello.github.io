2699b60b544b96fb962f6dd8e694c42b6b8d7de5
Samuel Puello <spuellocoronado@gmail.com>
2018-06-12 19:57:41 -0400

    Structuring text

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,21 @@
-var myHeading = document.querySelector('h1');
-var myButton = document.querySelector('button');
-var myImage=document.querySelector('img');
+var list = document.createElement('ul');
+var info = document.createElement('p');
+var html = document.querySelector('html');
 
-myImage.onclick = function (){
-	var mySrc = myImage.getAttribute('src');
-	if (mySrc==='images/firefox.jpeg'){
-		myImage.setAttribute('src','images/firefox2.jpg')
-	} else{
-		myImage.setAttribute('src','images/firefox.jpeg')
-	}
+info.textContent = 'Below is a dynamic list. Click anywhere outside the list to add a new list item. Click an existing list item to change its text to something else.';
+
+document.body.appendChild(info);
+document.body.appendChild(list);
+
+html.onclick = function() {
+  var listItem = document.createElement('li');
+  var listContent = prompt('What content do you want the list item to have?');
+  listItem.textContent = listContent;
+  list.appendChild(listItem);
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    var listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
 }
-
-function setUserName(){
-	var myName = prompt('Please enter your name.');
-	localStorage.setItem('name',myName);
-	myHeading.textContent='Mozilla is cool, ' + myName;
-}
-
-if (!localStorage.getItem('name')) {
-	setUserName();
-}else{
-	var storedName = localStorage.getItem('name');
-	myHeading.textContent='Mozilla is cool, ' + storedName;
-}
-
-myButton.onclick= function(){
-	setUserName();
-	}	
